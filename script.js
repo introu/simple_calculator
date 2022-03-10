50b6ec6a8bd6e3e6cd4587cf11cf82d0378618ca
introu <bart.kulak@gmail.com>
2022-03-10 02:35:15 +0100

    add basic button and screen functionality

--- script.js
+++ script.js
@@ -27,3 +27,41 @@ const operate = function (operator, a, b) {
         return divide(a, b)
     } else return 'ERROR'
 }
+
+const screen = document.getElementById('screen')
+const divideButton = document.getElementById('divide')
+const multiplyButton = document.getElementById('multiply')
+const equalsButton = document.getElementById('equals')
+const subtractButton = document.getElementById('subtract')
+const addButton = document.getElementById('add')
+const clearButton = document.getElementById('clear')
+const backButton = document.getElementById('back')
+/*
+const zeroButton = document.getElementById('zero')
+const oneButton = document.getElementById('one')
+const twoButton = document.getElementById('two')
+const threeButton = document.getElementById('three')
+const fourButton = document.getElementById('four')
+const fiveButton = document.getElementById('five')
+const sixButton = document.getElementById('six')
+const sevenButton = document.getElementById('seven')
+const eightButton = document.getElementById('eight')
+const nineButton = document.getElementById('nine')
+*/
+
+document.querySelectorAll('.number').forEach(item => {
+    item.addEventListener('click', event => {
+        if (screen.innerText === '0' && item.textContent === '0') {
+        } else if (screen.innerText === '0' && item.textContent === '.') {
+            screen.innerText += item.textContent
+        } else if (screen.innerText === '0' && item.textContent !== '0') {
+            screen.innerText = item.textContent;
+        } else if (screen.innerText.includes('.') && item.textContent === '.') {
+        } else screen.innerText += item.textContent;
+    })
+})
+
+
+clearButton.addEventListener('click', function () {
+    screen.innerText = '0';
+})
